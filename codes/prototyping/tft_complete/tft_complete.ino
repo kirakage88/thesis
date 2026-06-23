0c786054534f485b17a6633af6036bf2749408fa
kirakage888 <170931512+kirakage88@users.noreply.github.com>
2026-06-24 03:42:53 +0800

    Added Navigation Bar, Icons with Active and Inactive States, updated Design and Code

--- codes/prototyping/tft_complete/tft_complete.ino
+++ codes/prototyping/tft_complete/tft_complete.ino
@@ -4,10 +4,12 @@
 
 //Color Palette
 #define CL_TEXT 0x2104
-#define CL_SURFACE 0xf7be
+#define CL_ACCENT 0x424a
 #define CL_PRIMARY 0x31a6
 #define CL_SECONDARY 0xce59
-#define CL_ACCENT 0x424a
+#define CL_CONTAINER 0xdefb
+#define CL_SURFACE 0xf7be
+
 
 #define CD_TEXT 0xdefb
 #define CD_SURFACE 0x0841
@@ -19,6 +21,19 @@
 
 //Assets
 #include "assets/wifi.h"
+#include "assets/home.h"
+#include "assets/nav_container.h"
+
+#include "assets/pair.h"
+#include "assets/devices.h"
+#include "assets/units.h"
+#include "assets/system_info.h"
+
+#include "assets/en_pair.h"
+#include "assets/en_devices.h"
+#include "assets/en_units.h"
+#include "assets/en_system_info.h"
+//#include "assets/home_page.h" //Debug
 #define C_TRANSPARENT 0x0000
 
 //Fonts
@@ -28,17 +43,49 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
+
+struct NavIcon {
+    const uint16_t* bmp;
+    uint16_t x, y, w, h;
+};
+
+const NavIcon nav_icons[4][2] = {
+    // icon 0: pair
+    { { pair,      29, 280, 24, 34 },
+      { en_pair,   22, 280, 40, 33 } },
+    // icon 1: devices
+    { { devices,   74, 280, 38, 34 },
+      { en_devices,74, 280, 40, 33 } },
+    // icon 2: units
+    { { units,     133, 280, 25, 33 },
+      { en_units,  126, 280, 40, 33 } },
+    // icon 3: system_info
+    { { system_info,   180, 280, 35, 35 },
+      { en_system_info, 178, 280, 40, 35 } },
+};
+
+
+int nav_selected = 0;
+unsigned long last_switch = 0;
+
+
 //--------------------CODE HERE---------------------------//
 void setup() {
   Serial.begin(115200);
   tft.init();
   tft.setRotation(2);
 
-  wifiInit();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (millis() - last_switch >= 1000) {
+    last_switch = millis();
+    nav_selected++;
+    if (nav_selected > 4) nav_selected = 0;
+    homePage();
+  }
 }
 //-------------------------------------------------------//
 
@@ -54,3 +101,18 @@ void wifiInit(void) {
 }
 
 
+void homePage(void) {
+  //Background
+  tft.pushImage(0, 0, 240, 320, home);
+
+
+  //Navigation Bar
+  tft.pushImage(4, 275, 232, 42, nav_container, C_TRANSPARENT);
+
+  //tft.pushImage(0, 0, 240, 320, home_page);
+  for (int i = 0; i < 4; i++) {
+    int state = (nav_selected == i + 1) ? 1 : 0;
+    const NavIcon& icon = nav_icons[i][state];
+    tft.pushImage(icon.x, icon.y, icon.w, icon.h, icon.bmp, C_TRANSPARENT);
+  }
+}
